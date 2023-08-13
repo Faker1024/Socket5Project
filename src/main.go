0e801a9d3b46d074606b218b5cfed63744313151
Faker1024 <3361935899@qq.com>
2023-08-14 01:23:27 +0800

    main

--- src/main.go
+++ src/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
 
+	listen, err := net.Listen(`tcp`, `:1080`)
+	if err != nil {
+		fmt.Printf("Listen failed: %v\n", err)
+		return
+	}
+
+	for {
+		client, err := listen.Accept()
+		if err != nil {
+			fmt.Printf("Accept failed: %v", err)
+			continue
+		}
+		go process(client)
+	}
+
+}
+
+func process(client net.Conn) {
+	remoteAddr := client.RemoteAddr().String()
+	fmt.Printf("Connection from %s\n", remoteAddr)
+	_, err := client.Write([]byte("Hello world!\n"))
+	if err != nil {
+		fmt.Printf("write failed: %v", err)
+		return
+	}
+	client.Close()
 }
